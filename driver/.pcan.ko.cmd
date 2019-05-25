91fd598daafa59e5df2daeb7f27ceca26e024f0a
contact2saxena <contact2saxena@gmail.com>
2019-05-25 14:04:41 +0530

    This is the first commit for thr transmission of J1939 prackets over CAN using PEAK CAN module

---
+++ driver/.pcan.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ubuntu/Desktop/peak-linux-driver-8.7.0/driver/pcan.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/ubuntu/Desktop/peak-linux-driver-8.7.0/driver/pcan.ko /home/ubuntu/Desktop/peak-linux-driver-8.7.0/driver/pcan.o /home/ubuntu/Desktop/peak-linux-driver-8.7.0/driver/pcan.mod.o ;  true
